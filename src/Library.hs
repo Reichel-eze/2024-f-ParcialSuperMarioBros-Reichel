1f0d6a01c3ddf0e2483ee10e5b3a1aa305890b51
Reichel-eze <reicheleze@gmail.com>
2024-12-18 15:37:48 -0300

    termino el 1) punto

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,31 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+-- 1) Modelar a los plomeros y sus herramientas.
+
+data Plomero = UnPlomero {
+    nombre :: String,
+    dinero :: Number,
+    reparaciones :: Number,
+    herramientas :: [Herramienta]
+}deriving(Show, Eq)
+
+data Herramienta = UnaHerramienta {
+    denominacion :: String,
+    material :: Material,
+    precio :: Number
+}deriving(Show, Eq)
+
+data Material = Hierro | Madera | Goma | Plastico deriving(Show, Eq)
+
+-- Mario, un plomero que tiene $1200, no hizo ninguna reparación hasta ahora y en su caja de herramientas lleva una 
+-- llave inglesa con mango de hierro que tiene un precio de $200 y un martillo con empuñadura de madera que le salió $20.
+
+mario :: Plomero
+mario = UnPlomero "Mario" 1200 0 [(UnaHerramienta "llave inglesa" Hierro 200), (UnaHerramienta "martillo" Madera 20)]
+
+-- Wario, tiene 50 centavos encima, no hizo reparaciones, lleva infinitas llaves francesas, obviamente de hierro, la primera 
+-- le salió un peso, pero cada una que compraba le salía un peso más cara. La inflación lo está matando. 
+
+wario :: Plomero
+wario = UnPlomero "Wario" 0.50 0 (map (UnaHerramienta "llave francesa" Hierro) [1..])
